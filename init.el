cb1a75857719f72c03a379af10f22872772e854c
wuzehv <wuzehv@qq.com>
2017-12-13 13:35:44 +0800

    bugfix

--- init.el
+++ init.el
@@ -44,7 +44,7 @@ This function should only modify configuration layer settings."
      ;; emacs-lisp
      ;; git
      ;; markdown
-     ;;neotree
+     ;; neotree
      org
      ;; (shell :variables
      ;;        shell-default-height 30
@@ -356,18 +356,19 @@ This function is called at the very end of Spacemacs startup, after layer
 configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
-(xterm-mouse-mode 0)
+  (xterm-mouse-mode 0)
 
-;; optimize dired
-(put 'dired-find-alternate-file 'disabled nil)
-(with-eval-after-load 'dired
-      (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
+  ;; optimize dired
+  (put 'dired-find-alternate-file 'disabled nil)
+  (with-eval-after-load 'dired
+        (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
 
-;; config for org
-(setq org-bullets-bullet-list '("■" "◆" "▲" "▶"))
+  ;; config for org
+  (setq org-bullets-bullet-list '("■" "◆" "▲" "▶"))
+  )
+
 (setq custom-file (expand-file-name "custom.el" dotspacemacs-directory))
 (load custom-file 'no-error 'no-message)
-  )
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
@@ -376,18 +377,4 @@ before packages are loaded."
 This is an auto-generated function, do not modify its content directly, use
 Emacs customize menu instead.
 This function is called at the very end of Spacemacs initialization."
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (linum-relative ws-butler winum which-key web-mode web-beautify volatile-highlights vi-tilde-fringe uuidgen use-package unfill toc-org tagedit symon string-inflection spaceline slim-mode scss-mode sass-mode restart-emacs request rainbow-delimiters pug-mode popwin phpunit phpcbf php-auto-yasnippets persp-mode pcre2el password-generator paradox org-projectile org-present org-pomodoro org-download org-bullets org-brain open-junk-file neotree mwim move-text macrostep lorem-ipsum livid-mode link-hint less-css-mode json-mode js2-refactor js-doc info+ indent-guide impatient-mode hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation hide-comnt help-fns+ helm-themes helm-swoop helm-purpose helm-projectile helm-mode-manager helm-make helm-flx helm-descbinds helm-css-scss helm-company helm-c-yasnippet helm-ag google-translate golden-ratio gnuplot fuzzy flycheck-pos-tip flx-ido fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-org evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-exchange evil-escape evil-ediff evil-args evil-anzu eval-sexp-fu emmet-mode elisp-slime-nav editorconfig dumb-jump drupal-mode define-word company-web company-tern company-statistics company-php column-enforce-mode coffee-mode clean-aindent-mode auto-yasnippet auto-highlight-symbol auto-compile aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line ac-ispell))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 )
