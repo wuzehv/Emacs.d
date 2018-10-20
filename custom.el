b7843985bf70a5f31a88bb0dfe1b894066abb514
wuzehui <wuzehui@zuinianqing.com>
2018-10-20 09:45:15 +0800

    add config

--- custom.el
+++ custom.el
@@ -20,5 +20,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-done ((t (:inherit org-headline-done :foreground "#50fa7b" :strike-through t :weight bold))))
- '(org-headline-done ((t (:foreground "#50fa7b" :strike-through t)))))
+ '(org-done ((t (:inherit org-headline-done :foreground "dim gray" :strike-through t :weight bold))))
+ '(org-headline-done ((t (:foreground "dim gray" :strike-through t)))))
