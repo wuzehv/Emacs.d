99454fcd6ca39473bfd0f00e052b989104a785d9
wuzehui <wuzehui@zuinianqing.com>
2019-07-23 17:55:08 +0800

    config

--- emacs-world/better-default.el
+++ emacs-world/better-default.el
@@ -195,4 +195,18 @@
 
 (require 'company-tabnine)
 
+;; Trigger completion immediately.
+(setq company-idle-delay 0)
+
+;; Number the candidates (use M-1, M-2 etc to select completions).
+(setq company-show-numbers t)
+
+;; Use the tab-and-go frontend.
+;; Allows TAB to select and complete at the same time.
+(company-tng-configure-default)
+(setq company-frontends
+      '(company-tng-frontend
+        company-pseudo-tooltip-frontend
+        company-echo-metadata-frontend))
+
 (provide 'better-default)
