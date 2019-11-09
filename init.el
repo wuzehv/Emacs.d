250ee78c117b15955d4992458dfbed1309bf2e9f
wuzehv <wuzehv@qq.com>
2019-11-08 21:53:22 -0500

    update

--- init.el
+++ init.el
@@ -1,9 +1,3 @@
-(let* ((normal-gc-cons-threshold (* 20 1024 1024))
-      (init-gc-cons-threshold (* 128 1024 1024))
-	  (gc-cons-threshold init-gc-cons-threshold))
-  (add-hook 'emacs-startup-hook
-            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
-
 (add-hook 'after-init-hook
           (lambda ()
             (require 'server)
